287d0f033871aea2cfd3f7a34bc4d40dfa2f9baf
long1308 <lemle2580@gmail.com>
2023-09-14 13:04:27 +0700

    WBST-7-[BE]-CRUD-Product

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import productRouter from "./router/product.js";
 //config
 const app = express();
 const API_DB = process.env.API_DB;
@@ -12,9 +12,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 // router
-
-
-
+app.use("/api", productRouter);
 // database config
 mongoose.connect(API_DB);
 export const viteNodeApp = app;
