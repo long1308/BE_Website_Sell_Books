8c730455ccd05acff41b1b29d9e456119776b2df
long1308 <lemle2580@gmail.com>
2023-09-15 12:07:29 +0700

    WBST-8-[BE]-CRUD-Category

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import productRouter from "./router/product.js";
+import categoryRouter from "./router/category.js";
 //config
 const app = express();
 const API_DB = process.env.API_DB;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 // router
 app.use("/api", productRouter);
+app.use("/api", categoryRouter);
 // database config
 mongoose.connect(API_DB);
 export const viteNodeApp = app;
